54135ee9dffdd3753b3612e8899fd77617c3b603
John Wildes <johnwildes@live.com>
2024-01-21 14:22:38 -0600

    added Theme config and Navbar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,20 @@
 import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'en-US',
   title: 'Military Maritime Encyclopedia',
   description: 'Because US Navy Factbook is already taken',
+  theme: defaultTheme({
+    logo: './assets/mmelogo.png',
+    navbar: [
+      { text: 'Home', link: '/' },
+      { text: 'Ships', link: '/ships/' },
+      { text: 'Aircraft', link: '/aircraft/' },
+      { text: 'Subs', link: '/subs/' },
+      { text: 'Personnel', link: '/regulations/' },
+      { text: 'About', link: '/about/' }
+    ],
+  }),
   head: [['link', { rel: 'icon', href: './assets/MMEFavIco.ico' }]],
 })
